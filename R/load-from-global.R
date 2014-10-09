4d5ad231dfff782c66283786157f9e92d9a86a93
Stefano Romano <stefano.romano@gamesys.co.uk>
2014-10-09 13:42:20 +0100

    library_global, loadNamespace_global

--- R/load-from-global.R
+++ R/load-from-global.R
@@ -1,5 +1,10 @@
 #' @export
-
-load_from_global <- function(pkg, ...) {
-  eval(substitute(library(pkg, lib = capsule$global_lib, ...)))
+library_global <- function(pkg, ...) {
+  eval(substitute(pkg, lib = capsule$global_lib, ...))
 }
+
+#' @export
+loadNamespace_global <- function(pkg, ...) {
+  loadNamespace(pkg, lib = capsule$global_lib, ...)
+}
+
