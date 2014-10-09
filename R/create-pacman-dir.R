1ce3d094aa0a1dc2214473236dbcc501222e4fb0
Stefano Romano <stefano.romano@gamesys.co.uk>
2014-10-09 15:49:37 +0100

    packrat -> pacman

--- R/create-pacman-dir.R
+++ R/create-pacman-dir.R
@@ -1,18 +1,18 @@
 #' Create a packrat sub-directory
-#' 
+#'
 #' This function create a packrat sub-directory for a project.
-#' 
+#'
 #' @param path Root directory of project.
 #' @export
-create_packrat_subdir <- function(path = ".") {
+create_pacman_subdir <- function(path = ".") {
   path <- normalizePath(path, mustWork = TRUE)
-  copy_packrat_template(path)
+  copy_pacman_template(path)
 }
 
 # helper functions -------------------------------------------------------------------
 
-copy_packrat_template <- function(path) {
-  template <- system.file("templates", "packrat", package = "pacman")
+copy_pacman_template <- function(path) {
+  template <- system.file("templates", "pacman", package = "pacman")
   file.copy(template, path, recursive = TRUE)
   dir.create(local_lib_path(path), recursive = TRUE)
   invisible(TRUE)
